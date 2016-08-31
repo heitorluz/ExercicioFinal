703f86d48098c849458e22a1a0b072b97cd29ffe
Heitor Luz <heitor.dl@gmail.com>
2016-08-31 20:08:11 -0300

    Initial Commit

---
+++ ExercicioFinal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExercicioFinal
+//
+//  Created by Heitor Luz on 8/31/16.
+//  Copyright © 2016 Heitor Luz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ExercicioFinal/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ExercicioFinal
+//
+//  Created by Heitor Luz on 8/31/16.
+//  Copyright © 2016 Heitor Luz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
